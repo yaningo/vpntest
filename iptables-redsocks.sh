f6d7f3656c498aeb02e9bdaf5d9e4fb72619fd08
Yann D <yaningo@users.noreply.github.com>
2021-01-27 16:17:53 +0000

    Update iptables-redsocks.sh

--- iptables-redsocks.sh
+++ iptables-redsocks.sh
@@ -1,26 +1,15 @@
-#!/bin/bash
-# Create new chain
-sudo iptables -t nat -N REDSOCKS
+#!/bin/bash -x
+set -e
 
-# Ignore LANs and some other reserved addresses.
-sudo iptables -t nat -A REDSOCKS -d 0.0.0.0/8 -j RETURN
-sudo iptables -t nat -A REDSOCKS -d 10.0.0.0/8 -j RETURN
-sudo iptables -t nat -A REDSOCKS -d 127.0.0.0/8 -j RETURN
-sudo iptables -t nat -A REDSOCKS -d 169.254.0.0/16 -j RETURN
-sudo iptables -t nat -A REDSOCKS -d 172.16.0.0/12 -j RETURN
-sudo iptables -t nat -A REDSOCKS -d 192.168.0.0/16 -j RETURN
-sudo iptables -t nat -A REDSOCKS -d 224.0.0.0/4 -j RETURN
-sudo iptables -t nat -A REDSOCKS -d 240.0.0.0/4 -j RETURN
+function tunnel_up(){
+    ssh -o StrictHostKeyChecking=no -v openvpnas@ec2-54-246-54-223.eu-west-1.compute.amazonaws.com -22 -D 9999 -nf "sleep 90000" &
+    sudo redsocks -c ~/redsocks.conf &
+    sudo iptables -t nat -N REDSOCKS
+    sudo iptables -t nat -A REDSOCKS -p tcp -d 10.0.0.0/8 -j DNAT --to 127.0.0.1:12345
+    sudo iptables -t nat -A OUTPUT -d 10.0.0.0/8 -j REDSOCKS
+    sudo iptables -t nat -I PREROUTING 1 -d 10.0.0.0/8 -j REDSOCKS
+}
 
-# Put your ssh server IP here
-sudo iptables -t nat -A REDSOCKS -d 54.246.54.223/4 -j RETURN
-
-# Anything else should be redirected to port 31338
-sudo iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 31338
-
-# Any tcp connection made by `user' should be redirected, put your username here.
-sudo iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner circleci -j REDSOCKS
-
-redsocks -c redsocks.conf
-
-ssh -fND localhost:31337 user@server 2>/dev/null 1>2
+if [ $1 == "start" ]; then
+    tunnel_up
+fi
